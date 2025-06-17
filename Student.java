c472fb959a699b91c99205b304bd85a55a94ef1d
245122749024 <245122749024@mvsrec.edu.in>
2025-06-17 13:20:27 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+class Student 
+ { 
+  public static void main(String args[])
+  { 
+    String name = args[0];
+    int roll = Integer.parseInt(args[1]);
+    System.out.println("Student Name: " + name);
+    System.out.println("Student Roll Number: " + roll);
+    }
+}
+    
